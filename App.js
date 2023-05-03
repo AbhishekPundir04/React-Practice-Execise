cc169b124db568a5f9b0cb0e905c1277eef71c98
Abhishek Pundir <122037868+AbhishekPundir04@users.noreply.github.com>
2023-05-03 12:44:59 +0530

    App file

---
+++ App.js
@@ -0,0 +1,86 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+// import StateExample from "./useHookExamples/StateExample";
+// import Todo from "./useHookExamples/todoListUsingState.js/todoList."
+
+
+// import UseEffect from "./useHookExamples/useEffectAPI/UseEffectEx1"
+
+// import UseEffect from "./useHookExamples/useEffectAPI/UseEffectEx1"
+
+// import AppFile from "./useHookExamples/LifecycleOrUseEffect/AppFile"
+
+// import LifeCycle from "./useHookExamples/LifecycleOrUseEffect/LiifeCyclewithEffect"
+
+// import StateExample3 from "./useHookExamples/StateExample3"
+
+// import StateExample2 from "./useHookExamples/StateExmaple2"
+
+// import StateExample4 from "./useHookExamples/StateExample4" 
+
+// import UseEffectExTwo from "./useHookExamples/useEffectAPI/ExercisePredictAge"
+// import Excuse from "./useHookExamples/useEffectAPI/Exercise2"  
+
+// import Home from "./ReactRouters/Home"
+// import Menu from "./ReactRouters/Menu"
+// import Contact from "./ReactRouters/ContactUs"
+// import Navbar from "./ReactRouters/Navbar"
+
+const App = (props) =>{
+  
+  
+  return (
+    <div className="App-header">
+
+      {/* <Router>
+        <div>
+         <Navbar/>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/menu" element={<Menu/>}/>
+          <Route path="/contactus" element={<Contact/>}/>
+          <Route path="*" element={<h1>Page is not found</h1>}/>
+        </Routes>
+      </Router> */}
+
+     {/* <StateExample/> */}
+     {/* <StateExample2/> */}
+     {/* <StateExample3/> */}
+     {/* <StateExample4/> */}
+     {/* <Todo /> */}
+     {/* <LifeCycle/> */}
+     {/* <AppFile/> */}
+     {/* <UseEffect/> */}
+     {/* <UseEffectExTwo/> */}
+     {/* <Excuse/> */}
+
+    </div>
+  )
+}
+
+export default App
+
+
+// import Header from "./newHeader";
+
+// const App = (props) => {
+//   const value = [
+//     { name: "Mars", isGasPlanet: true },
+//     { name: "Earth", isGasPlanet: false },
+//     { name: "Jupiter", isGasPlanet: false },
+//     { name: "Uranus", isGasPlanet: true },
+//   ];
+
+//   return (
+//     <div>
+//       {value.map((value, key) => {
+//         return value.isGasPlanet ? (
+//           <Header key={key} name={value.name} isGasPlanet={value.isGasPlanet} />
+//         ) : null;
+//       })}
+//     </div>
+//   );
+// };
+
+// export default App;
